3d496efc688c1221e647b3700ed1c857af93a588
Néstor Manuel <nestor706@gmail.com>
2020-04-19 19:50:10 -0300

    PS4: Base file

---
+++ finpy/ps4/ps/ps4.py
@@ -0,0 +1,19 @@
+"""
+Problem Set 4
+"""
+from math import exp, sqrt
+from random import gauss
+
+
+def sim_gbm(s0, drift, sigma, plazo):
+    """
+    Simula precio terminal de un Geometric Brownian Motion al cabo de un plazo.
+    Parámetros:
+        s0    : Precio inicial del stock
+        drift : Drift (Tasa diaria anualizada / Cap. contínua)
+        sigma : Volatidad (Diara anualizada / Cap. contínua)
+        plazo : Cantidad de días de plazo de la simulación
+    """
+    st = s0 * exp((drift - .5 * sigma * sigma) * (plazo / 365.) +
+                  sigma * sqrt(plazo / 365.) * gauss(0., 1.))
+    return st
