f7f10a3a7a625388e0d7dcb138094b31ed1d7376
Néstor Manuel <nestor706@gmail.com>
2020-04-21 10:03:46 -0300

    PS4: Class structure

--- finpy/ps4/ps/ps4.py
+++ finpy/ps4/ps/ps4.py
@@ -1,8 +1,30 @@
+# -*- coding: utf-8 -*-
 """
 Problem Set 4
 """
+
 from math import exp, sqrt
 from random import gauss
+
+
+class EuroDerivative:
+    pass
+
+
+class Strategy(EuroDerivative):
+    pass
+
+
+class VanillaOption(EuroDerivative):
+    pass
+
+
+class Call(VanillaOption):
+    pass
+
+
+class Put(VanillaOption):
+    pass
 
 
 def sim_gbm(s0, drift, sigma, plazo):
